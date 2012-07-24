9bc65fd4c6aa95181e63bada01e4d8afc67e0f89
m2m tech <tech@m2m.at>
2012-07-24 17:54:00 +0200

    Initial Commit

---
+++ Shutterbug/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Shutterbug
+//
+//  Created by Martin Mandl on 24.07.12.
+//  Copyright (c) 2012 m2m. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Shutterbug/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Shutterbug
+//
+//  Created by Martin Mandl on 24.07.12.
+//  Copyright (c) 2012 m2m. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
